9311d900b852133dcdae13aaf213461ff2b04521
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-03-15 09:57:42 +0500

    update some files

---
+++ src/BookStore.Presentation/MVC/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Application.useCases.Authors.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AuthorController(IMediator mediator)
+            => _mediator = mediator;
+
+        public async Task<IActionResult> Index()
+        {
+            var query = new GetAllAuthorsQuery();
+            var authors = await _mediator.Send(query);
+
+            ViewData["authors"] = authors;
+            ViewBag.Authors = authors;
+            return View();
+        }
+    }
+}
